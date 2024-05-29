7e98e11e86e60987edd9a78d6a471d6cb0ec349b
Fredericjean <fred42110@hotmail.fr>
2024-05-29 13:49:39 +0200

    feat(user): "add user entity/constraint validation/login page/security

---
+++ src/Controller/BackEnd/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\BackEnd;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/users', name: 'admin.users')]
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
