dceb832bc549d468a5ca981f88edc1d00ef601d7
Fredericjean <fred42110@hotmail.fr>
2024-05-30 14:54:05 +0200

    feat(admin): "add security for admin+ redirect route if admin

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller\Security;
 
 use App\Entity\User;
@@ -7,7 +6,7 @@ use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -17,25 +16,34 @@ class SecurityController extends AbstractController
     #[Route('/login', name: 'app.login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        $error= $authenticationUtils ->getLastAuthenticationError();
-        $lastUserName= $authenticationUtils ->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('Security/login.html.twig', [
-            'lastUsername' => $lastUserName,
-            'error' =>$error,
+            'lastUsername' => $lastUsername,
+            'error' => $error,
         ]);
     }
-    
-    
-    #[Route('register', name:'app.register')]
-    public function register(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em ): Response 
+
+    #[Route('/post-login', name: 'app.post_login')]
+    public function postLogin(): Response
+    {
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin.users.index');
+        } else {
+            return $this->redirectToRoute('frontend.home');
+        }
+    }
+
+    #[Route('/register', name: 'app.register')]
+    public function register(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
     {
         $user = new User();
 
-        $form= $this ->createForm(UserType::class, $user);
+        $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $user->setPassword(
                 $hasher->hashPassword($user, $form->get('password')->getData())
             );
@@ -46,10 +54,9 @@ class SecurityController extends AbstractController
             $this->addFlash('success', 'Bienvenue :)');
             return $this->redirectToRoute('app.login');
         }
+
         return $this->render('Security/register.html.twig', [
-            'form' => $form, ]);
+            'form' => $form,
+        ]);
     }
-
-    
-
 }
